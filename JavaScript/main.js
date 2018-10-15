90a3239145f4b88575e0b928e5dc1f0ff9ba0656
Ingi Ingason <ingiingason@MacBook-Pro.2t.local>
2018-10-15 16:05:16 +0000

    prufa

---
+++ JavaScript/main.js
@@ -0,0 +1,19 @@
+var hello = "halló" + " heimur";
+/*
+Þetta er rosalega 
+langt comment...
+*/
+var test = !("jón"!=="Jón";
+var test2 = "jón"==="Jón";
+var bool = true; 
+
+// || eða (or)
+// && og (and)
+// ! ekki (not)
+
+var JavaScriptErSpennandi = true;
+var thettaErOmurlegurDagur = false;
+
+var test3 = JavaScriptErSpennandi && thettaErOmurlegurDagur; //false
+var test4 = JavaScriptErSpennandi || thettaErOmurlegurDagur; //true
+var test5 = !thettaErOmurlegurDagur; //true
